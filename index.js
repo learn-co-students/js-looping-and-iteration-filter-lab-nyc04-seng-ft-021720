4b80c67ff7701ebb1159e3d4ff333ae4793f1e7a
96StanleyLee <96stanleylee@gmail.com>
2020-03-27 23:04:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+
+
+function findMatching(array, name){
+    return array.filter(function (person) {return person.toLowerCase() === name.toLowerCase()})    
+}
+
+function fuzzyMatch(array, letters){
+    let length = letters.length 
+    return array.filter(function (driver) {return driver.slice(0,length) === letters})
+}
+
+function matchName(object, name){
+
+
+
+    return object.filter(function (driver) {return driver.name === name})
+    
+
+
+}
