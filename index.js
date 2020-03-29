895c83ce73382d240cdaa759aca5c8cc92193044
github username <github email address>
2020-03-29 14:17:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file
+function findMatching(array, name){
+    return array.filter(function (person) {return person.toLowerCase() === name.toLowerCase()})    
+}
+
+function fuzzyMatch(array, letters) {
+    return array.filter(function (driver) {return driver.slice(0,letters.length) === letters})
+}
+
+function matchName(object, name){
+    return object.filter(function (driver) {return driver.name === name})
+}
