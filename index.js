0a409f8cf7086a77572b71439d2a8127283e89ac
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-03-29 22:39:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file
+function findMatching(drivers, string){
+	return drivers.filter(function(driver){
+		return driver.toLowerCase() === string.toLowerCase();
+	});
+}
+
+function fuzzyMatch(drivers, string){
+	const pattern = new RegExp(`^${string}.+`,'i');
+	return drivers.filter(driver => pattern.test(driver));
+}
+
+function matchName(drivers, string){
+	return drivers.filter(driver => driver.name === string);
+}
