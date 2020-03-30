a868f168b034f815edb3b50d3439676a969f7cc8
Sir Eric <echeungsk@gmail.com>
2020-03-30 03:07:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(drivers, name){
+   return drivers.filter(function (driver) {
+     return driver.toLowerCase() === name.toLowerCase()
+   });
+}
+
+function fuzzyMatch(drivers, letters) {
+  let reg = new RegExp(`${letters}|\\w+`, `ig`);
+  return drivers.filter(function (driver) {
+    return driver !== driver.match(reg).toString()});
+}
+
+
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
